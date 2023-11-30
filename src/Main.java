418f0045f05b45b6bc7b8d77560f24d2f0c19f0d
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-11-29 20:37:32 -0800

    Add test

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    WebDriver driver;
+
+    public void launchBrowser(){
+        System.setProperty("webdriver.chrome.driver", "/Users/javier/chromedriver-mac-arm64/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/");
+    }
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        Main obj = new Main();
+        obj.launchBrowser();
     }
 }
